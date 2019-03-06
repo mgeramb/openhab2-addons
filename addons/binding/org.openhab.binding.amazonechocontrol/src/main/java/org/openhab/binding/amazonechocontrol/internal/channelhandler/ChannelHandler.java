87f55eca1f4afaf43f441cfcf4c98eec47d0fee7
Michael Geramb <mail@michael-geramb.at>
2019-03-06 21:41:13 +0100

    [amazonechocontrol] Announcement channel Signed-off-by: Michael Geramb <mail@michael-geramb.at> (github: mgeramb)

--- addons/binding/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/channelhandler/ChannelHandler.java
+++ addons/binding/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/channelhandler/ChannelHandler.java
@@ -11,9 +11,16 @@ package org.openhab.binding.amazonechocontrol.internal.channelhandler;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.types.Command;
 import org.openhab.binding.amazonechocontrol.internal.Connection;
+import org.openhab.binding.amazonechocontrol.internal.handler.IAmazonThingHandler;
 import org.openhab.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 /**
  * The {@link ChannelHandler} is the base class for all channel handlers
@@ -23,4 +30,28 @@ import org.openhab.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
 public abstract class ChannelHandler {
     public abstract boolean tryHandleCommand(Device device, Connection connection, String channelId, Command command)
             throws IOException, URISyntaxException;
+
+    protected final IAmazonThingHandler thingHandler;
+    protected final Gson gson = new Gson();
+    private final Logger logger;
+
+    protected ChannelHandler(IAmazonThingHandler thingHandler) {
+        this.logger = LoggerFactory.getLogger(this.getClass());
+        this.thingHandler = thingHandler;
+    }
+
+    protected <T> @Nullable T tryParseJson(String json, Class<T> type) {
+        try {
+            return gson.fromJson(json, type);
+        } catch (JsonSyntaxException e) {
+            logger.debug("Json parse error {}", e);
+            return null;
+        }
+    }
+
+    protected <T> @Nullable T parseJson(String json, Class<T> type) throws JsonSyntaxException {
+        return gson.fromJson(json, type);
+
+    }
+
 }
--- addons/binding/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/channelhandler/ChannelHandlerAnnouncement.java
+++ addons/binding/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/channelhandler/ChannelHandlerAnnouncement.java
@@ -11,11 +11,14 @@ package org.openhab.binding.amazonechocontrol.internal.channelhandler;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.types.Command;
 import org.openhab.binding.amazonechocontrol.internal.Connection;
 import org.openhab.binding.amazonechocontrol.internal.handler.IAmazonThingHandler;
 import org.openhab.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
+
+import com.google.gson.JsonSyntaxException;
 
 /**
  * The {@link ChannelHandlerAnnouncement} is responsible for the announcement channel
@@ -25,10 +28,8 @@ import org.openhab.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
 public class ChannelHandlerAnnouncement extends ChannelHandler {
     public static final String CHANNEL_NAME = "announcement";
 
-    IAmazonThingHandler thingHandler;
-
     public ChannelHandlerAnnouncement(IAmazonThingHandler thingHandler) {
-        this.thingHandler = thingHandler;
+        super(thingHandler);
     }
 
     @Override
@@ -37,12 +38,27 @@ public class ChannelHandlerAnnouncement extends ChannelHandler {
         if (channelId.equals(CHANNEL_NAME)) {
             if (command instanceof StringType) {
                 String commandValue = ((StringType) command).toFullString();
-                String text = "<speak></speak>";
-                if (commandValue.startsWith("!")) {
-                    commandValue = commandValue.substring(1);
-                    text = "";
+                String body = commandValue;
+                String title = null;
+                String speak = " "; // blank generates a beep
+                if (commandValue.startsWith("{") && commandValue.endsWith("}")) {
+                    try {
+                        AnnouncementRequestJson request = parseJson(commandValue, AnnouncementRequestJson.class);
+                        if (request != null) {
+                            title = request.title;
+                            body = request.body;
+                            if (body == null) {
+                                body = "";
+                            }
+                            if (request.sound == false) {
+                                speak = "<speak></speak>";
+                            }
+                        }
+                    } catch (JsonSyntaxException e) {
+                        body = e.getLocalizedMessage();
+                    }
                 }
-                connection.sendAnnouncement(device, text, commandValue, null, 0, 0);
+                connection.sendAnnouncement(device, speak, body, title, 0, 0);
             }
             RefreshChannel();
         }
@@ -51,6 +67,12 @@ public class ChannelHandlerAnnouncement extends ChannelHandler {
 
     void RefreshChannel() {
         thingHandler.updateChannelState(CHANNEL_NAME, new StringType(""));
+    }
+
+    class AnnouncementRequestJson {
+        public @Nullable Boolean sound;
+        public @Nullable String title;
+        public @Nullable String body;
     }
 
 }
